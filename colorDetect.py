11c2a2e57ac2b1fb71f25a8b2dd160f0d6e85531
Diego Villamil <dmvillamil@up.edu.ph>
2024-01-08 20:06:00 +0800

    added color quantization

--- colorDetect.py
+++ colorDetect.py
@@ -36,8 +36,28 @@ def colorDetect(dir):
     #load image and convert to hsv
     path = "images/" + filename
     rgb_img = cv2.imread(path)
+
+    # Color Quantization taken from 
+    # https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_ml/py_kmeans/py_kmeans_opencv/py_kmeans_opencv.html#kmeans-opencv
+    Z = rgb_img.reshape((-1,3))
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 4
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((rgb_img.shape))
+
+    cv2.imshow('res2',res2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     hsv_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
-    
     #Count the number of pixels for each color
     for key in color_count:
       if(key == 'red'):

2b02601c062bdb91f4629071abe8e06832415f8f
Diego Villamil <dmvillamil@up.edu.ph>
2024-01-09 17:42:19 +0800

    inpaints to reduce the reflections and saves the restuls to a csv file

--- colorDetect.py
+++ colorDetect.py
@@ -1,22 +1,61 @@
 import cv2
+import csv
 import numpy as np
 import os
 
+# image resize while keeping aspect ratio taken from:
+# https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+# answered by thewaywewere, edited by starball
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 def colorDetect(dir):
   files = os.listdir(dir)
-  # HSV ranges for each color taken from 
-  # https://stackoverflow.com/questions/36817133/identifying-the-range-of-a-color-in-hsv-using-opencv
+  fields = ['Filename', 'Color']
+  rows = []
 
+  # Original HSV ranges for each color taken from 
+  # https://stackoverflow.com/questions/36817133/identifying-the-range-of-a-color-in-hsv-using-opencv
+  # some values have been changed and pink was added
+  
   color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
                 'white': [[180, 18, 255], [0, 0, 231]],
-                'red1': [[180, 255, 255], [166, 50, 70]],
                 'red2': [[9, 255, 255], [0, 50, 70]],
-                'green': [[89, 255, 255], [36, 50, 70]],
-                'blue': [[128, 255, 255], [90, 50, 70]],
-                'yellow': [[35, 255, 255], [25, 50, 70]],
-                'purple': [[144, 255, 255], [129, 50, 70]],
-                'pink': [[165,255,255], [145, 50, 70]],
                 'orange': [[24, 255, 255], [10, 50, 70]],
+                'yellow': [[35, 255, 255], [25, 50, 70]],
+                'green': [[89, 255, 255], [36, 50, 70]],
+                'blue': [[114, 255, 255], [90, 50, 70]],
+                'purple': [[144, 255, 255], [115, 50, 70]],
+                'pink': [[175,255,255], [145, 50, 70]],
+                'red1': [[180, 255, 255], [176, 50, 70]],                
                 'gray': [[180, 18, 230], [0, 0, 40]]}
 
   color_count = {'black': 0,
@@ -32,14 +71,33 @@ def colorDetect(dir):
 
   for filename in files:
 
-    #load image and convert to hsv
+    # load image and convert to hsv
     path = "images/" + filename
     rgb_img = cv2.imread(path)
-    hsv_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
+    width = rgb_img.shape[1]
+    height = rgb_img.shape[0]
+
+    # resize if the image is too large
+    if (width > 1024):
+      rgb_img = image_resize(rgb_img, width=1024)
+    if (height > 576):
+      rgb_img = image_resize(rgb_img, height=576)
     
-    #Count the number of pixels for each color
+    # no post processing version of the image 
+    nopp_hsv_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
+
+    # Reflection reduction through inpainting taken from:
+    # https://itecnote.com/tecnote/opencv-reflection-reduction-in-image/
+    gray = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5,5), 0)
+    thresh = cv2.threshold(blurred, 210, 255, cv2.THRESH_BINARY)[1]
+    inpaint_img = cv2.inpaint(rgb_img,thresh,3,cv2.INPAINT_TELEA)
+
+    hsv_img = cv2.cvtColor(inpaint_img, cv2.COLOR_BGR2HSV)
+    
+    # Count the number of pixels for each color
     for key in color_count:
-      if(key == 'red'):
+      if (key == 'red'):
         upper = tuple(color_dict_HSV['red1'][0])
         lower = tuple(color_dict_HSV['red1'][1])
 
@@ -50,15 +108,30 @@ def colorDetect(dir):
         lower = tuple(color_dict_HSV['red2'][1])
 
         mask = cv2.inRange(hsv_img,lower,upper)
-        color_count[key] += np.count_nonzero(mask)
+        color_count[key] += np.count_nonzero(mask)  
       else:
         upper = tuple(color_dict_HSV[key][0])
         lower = tuple(color_dict_HSV[key][1])
 
-        mask = cv2.inRange(hsv_img,lower,upper)
+        if (key == 'white'):
+          mask = cv2.inRange(nopp_hsv_img,lower,upper)
+        else:
+          mask = cv2.inRange(hsv_img,lower,upper)
         color_count[key] = np.count_nonzero(mask)
       
     #get the color with the most number of pixels in the image
     car_color = max(color_count, key= lambda x:color_count[x])
-    print("The color of the car in " , filename, " is ", car_color)
+    rows.append([filename,car_color])
+    #print("The color of the car in " , filename, " is ", car_color)
+    
 
+    #display image with the identified color as the window name
+    # cv2.imshow(car_color,inpaint_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+  
+  with open('data.csv', 'w') as f:
+    write = csv.writer(f)
+
+    write.writerow(fields)
+    write.writerows(rows)
